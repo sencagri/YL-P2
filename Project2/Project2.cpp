8ffe8ed67e1ac708e2ac6160834402643eaf2d69
Ebubekir Çağrı ŞEN <cagrisen_snt@hotmail.com>
2017-10-16 08:49:47 +0300

    2-dimensional filter feature implemented

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -8,6 +8,7 @@
 #include "opencv2\opencv.hpp"
 #include "opencv\highgui.h"
 #include "histogram.cpp"
+#include "filter.cpp"
 
 using namespace cv;
 using namespace std;
@@ -18,8 +19,17 @@ int main()
 	int cmd = 0;
 
 	Mat image = imread("c:\\fig309.tif", IMREAD_GRAYSCALE);
+	Mat result = image.clone();
+	Mat_<float> kernel(3, 3);
 
+	float val = 1.0f / 9;
 
+	kernel << val, val, val, val, val, val, val, val, val;
+
+	applyFilter(image, kernel, result);
+
+	imshow("Processed Image", result);
+	imshow("Original Image", image);
 
 	waitKey(0);
 	getchar();
--- Project2/filter.cpp
+++ Project2/filter.cpp
@@ -1,8 +1,53 @@
-#include "stdafx.h"
+﻿#include <stdio.h>
 #include <iostream>
-#include <stdio.h>
 #include <direct.h>
+#include <vector>
+
+#include "stdafx.h"
+#include "math.h"
+
 #include "opencv2\opencv.hpp"
 #include "opencv\highgui.h"
 
+using namespace cv;
+using namespace std;
 
+void applyFilter(Mat & image, Mat_<float> kernel, Mat & result)
+{
+	// This is constant k, points to 𝑔(𝑥,𝑦)=𝑓(𝑥,𝑦)+𝑘 ∗[𝑓(𝑥,𝑦)−𝑓̅(𝑥,𝑦)]
+	float k(0.0);
+	cout << "Enter constant value for spatial filtering";
+	cin >> k;
+
+	uchar * kernelPrevP = kernel.ptr<uchar>(0);
+	uchar * kernelCurrP = kernel.ptr<uchar>(1);
+	uchar * kernelNextP = kernel.ptr<uchar>(2);
+
+	for (unsigned int i = 0; i < image.rows; i++)
+	{
+		// Input image row 
+		uchar * iCurr = image.ptr<uchar>(i);
+		uchar * iPrev(0);
+		uchar * iNext(0);
+		if (i - 1 > -1)
+		{
+			iPrev = image.ptr<uchar>(i - 1);
+		}
+		else if (i + 1 <= image.rows)
+		{
+			iNext = image.ptr<uchar>(i + 1);
+		}
+
+		// Output image row
+		uchar * oCurr = image.ptr<uchar>(i);
+
+		for (unsigned int j = 0; i < image.cols; j++)
+		{
+			float val = 1.0f * (iPrev[j] * kernelPrevP[0]) + (iPrev[j + 1] * kernelPrevP[1]) + (iPrev[j + 1] * kernelPrevP[j + 2]) +
+				(iCurr[j] * kernelCurrP[0]) + (iCurr[j + 1] * kernelCurrP[1]) + (iCurr[j + 1] * kernelCurrP[j + 2]) +
+				(iNext[j] * kernelNextP[0]) + (iNext[j + 1] * kernelNextP[1]) + (iNext[j + 1] * kernelNextP[j + 2]);
+
+			oCurr[j] = (int)val;
+		}
+	}
+}
